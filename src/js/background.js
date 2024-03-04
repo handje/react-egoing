993710f95a8fa9ac758b195fd6f48f1f69020fa9
Sonjieun <sonjieun2661@gmail.com>
2024-03-04 18:22:38 +0900

    reset

--- src/js/background.js
+++
@@ -1,7 +0,0 @@
-const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
-
-const todayBackground = images[Math.floor(Math.random() * images.length)];
-
-const background = document.createElement("img");
-background.src = `src/img/${todayBackground}`;
-document.body.appendChild(background);
